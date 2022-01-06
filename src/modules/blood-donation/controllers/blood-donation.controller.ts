d692d58523f6c66c557d139f3e6506a6dae165a3
Kiet Nguyen <kietngt00@gmail.com>
2022-01-06 17:34:02 +0700

    api: get all donation

--- src/modules/blood-donation/controllers/blood-donation.controller.ts
+++ src/modules/blood-donation/controllers/blood-donation.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { UserEntity } from './../../user/entities/user.entity';
+import { BloodDonationService } from './../services/blood-donation.service';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from 'src/common/user.decorator';
+@UseGuards(AuthGuard())
+@ApiTags('Blood Donation')
+@ApiBearerAuth()
 @Controller('blood-donation')
-export class BloodDonationController {}
+export class BloodDonationController {
+  constructor(private readonly service: BloodDonationService) {}
+
+  @Get()
+  async getAllDonations(@CurrentUser() user: UserEntity) {
+    return await this.service.getAllDonations(user.id);
+  }
+}
